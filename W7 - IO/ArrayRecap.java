e2922ac54580acbbe2ae0993f5ac51d8f6d2cc2b
David Amiel <david.amiel@rutgers.edu>
2025-03-06 16:06:04 -0500

    Week 7, IO Code

--- W7 - IO/ArrayRecap.java
+++ W7 - IO/ArrayRecap.java
@@ -10,13 +10,30 @@ public class ArrayRecap {
         // For each row, visit each column. Add each value to a sum, then get the row-average
         // by dividing that sum by the length of the row.
 
+        double[] averages = new double[grades.length];
+
         for(int i = 0; i < grades.length; i++) {
             double sum = 0;
             for(int j = 0; j< grades[i].length; j++) {
                 sum += grades[i][j];
             }
-            System.out.println("Average of row " + i + " = " + sum/grades[i].length);
+            averages[i] = sum/grades[i].length;
         }
+
+
+        // Get col-wise average (the average of the numbers in each col)
+        // For each col, visit each row. Add each value to a sum, then get the col-average
+        // by dividing that sum by the length of the col.
+
+        for(int col = 0; col < grades[0].length; col++) {
+            double sum = 0;
+            for(int row = 0; row < grades.length; row++){
+                sum += grades[row][col];
+            }
+            System.out.println("Average of col "  + col + " = " + sum/grades[0].length);
+
+        }
+
 
 
 
