52e173541d42f942e9924d49f0361c291257f8be
David Amiel <david.amiel@rutgers.edu>
2025-03-06 16:05:20 -0500

    Week 7, IO Code

--- W7 - IO/ReadAlls.java
+++ W7 - IO/ReadAlls.java
@@ -1,10 +1,9 @@
 public class ReadAlls {
-
     public static void main(String[] args) {
         
         int[] a = StdIn.readAllInts();
         for(int element : a) {
-            StdOut.print(element);
+            StdOut.println(element);
         }
     }
     
